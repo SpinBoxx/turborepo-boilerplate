0298be3881fa16cbee69f995150f30260d588e4a
Spinbox <quentin.mimault@orange.fr>
2025-12-15 19:26:50 +0100

    fix(api): fix cors origin

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -7,7 +7,7 @@ import fastifyCors from "@fastify/cors";
 import Fastify from "fastify";
 
 const baseCorsConfig = {
-	origin: process.env.CORS_ORIGIN || "",
+	origin: process.env.CORS_ORIGIN || "*",
 	methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 	allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
 	credentials: true,
