632b624e778b3d440ce4e708aa2c3526ae65f39e
Spinbox <quentin.mimault@orange.fr>
2025-12-15 19:30:24 +0100

    feat(api): Added auth routes

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 import { createServer } from "node:http";
 import fastifyCors from "@fastify/cors";
 
-// import { auth } from "@zanadeal/auth";
+import { auth } from "@zanadeal/auth";
 import Fastify from "fastify";
 
 const baseCorsConfig = {
@@ -73,34 +73,34 @@ const fastify = Fastify({
 
 fastify.register(fastifyCors, baseCorsConfig);
 
-// fastify.route({
-// 	method: ["GET", "POST"],
-// 	url: "/api/auth/*",
-// 	async handler(request, reply) {
-// 		try {
-// 			const url = new URL(request.url, `http://${request.headers.host}`);
-// 			const headers = new Headers();
-// 			Object.entries(request.headers).forEach(([key, value]) => {
-// 				if (value) headers.append(key, value.toString());
-// 			});
-// 			const req = new Request(url.toString(), {
-// 				method: request.method,
-// 				headers,
-// 				body: request.body ? JSON.stringify(request.body) : undefined,
-// 			});
-// 			const response = await auth.handler(req);
-// 			reply.status(response.status);
-// 			response.headers.forEach((value, key) => reply.header(key, value));
-// 			reply.send(response.body ? await response.text() : null);
-// 		} catch (error) {
-// 			fastify.log.error({ err: error }, "Authentication Error:");
-// 			reply.status(500).send({
-// 				error: "Internal authentication error",
-// 				code: "AUTH_FAILURE",
-// 			});
-// 		}
-// 	},
-// });
+fastify.route({
+	method: ["GET", "POST"],
+	url: "/api/auth/*",
+	async handler(request, reply) {
+		try {
+			const url = new URL(request.url, `http://${request.headers.host}`);
+			const headers = new Headers();
+			Object.entries(request.headers).forEach(([key, value]) => {
+				if (value) headers.append(key, value.toString());
+			});
+			const req = new Request(url.toString(), {
+				method: request.method,
+				headers,
+				body: request.body ? JSON.stringify(request.body) : undefined,
+			});
+			const response = await auth.handler(req);
+			reply.status(response.status);
+			response.headers.forEach((value, key) => reply.header(key, value));
+			reply.send(response.body ? await response.text() : null);
+		} catch (error) {
+			fastify.log.error({ err: error }, "Authentication Error:");
+			reply.status(500).send({
+				error: "Internal authentication error",
+				code: "AUTH_FAILURE",
+			});
+		}
+	},
+});
 
 fastify.get("/", async () => {
 	return "OK";
