c4302e9b7eb3d5838046266f40ae468e721e45f6
Spinbox <quentin.mimault@orange.fr>
2026-01-06 00:55:04 +0100

    feat(admin): Redirect on /login if connected

--- apps/admin/src/routes/login/route.tsx
+++ apps/admin/src/routes/login/route.tsx
@@ -1,8 +1,17 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 import AuthLayout from "@/auth/components/AuthLayout";
 
 export const Route = createFileRoute("/login")({
 	component: RouteComponent,
+	beforeLoad: async ({ context }) => {
+		const user = await context.auth.loadSession();
+
+		if (user) {
+			throw redirect({
+				to: "/dashboard",
+			});
+		}
+	},
 });
 
 function RouteComponent() {
