a9339b3ee2db2a3e46a65630561b34a70b5d5b5a
quentin-mimo <quentin@rhyzom.io>
2026-03-11 12:32:10 +0100

    feat(web): Added guest step in BookingSearchBar

--- apps/web/src/features/booking/BookinSearchBar/BookingSearchBarMobile.tsx
+++ apps/web/src/features/booking/BookinSearchBar/BookingSearchBarMobile.tsx
@@ -1,7 +1,7 @@
 import { format } from "date-fns";
+import { motion } from "framer-motion";
 import { CalendarIcon, ChevronDown, Save } from "lucide-react";
 import { useMemo, useState } from "react";
-
 import { Button } from "@/components/ui/button";
 import {
 	Popover,
@@ -13,6 +13,7 @@ import {
 import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
 import { useBookingStore } from "../hooks/useBookingHook";
+import BookingGuestCountInput from "../ui/BookingGuestCountInput";
 import { BookingSearchBarCalendar } from "./BookingSearchBarCalendar";
 
 interface Props {
@@ -23,6 +24,7 @@ export default function BookingSearchBarMobile({ className }: Props) {
 	const { checkInDate, checkOutDate } = useBookingStore();
 
 	const [isOpen, setIsOpen] = useState(false);
+	const [step, setStep] = useState<"dates" | "guests">("dates");
 
 	const formatDate = useMemo(() => {
 		if (!checkInDate) return "When are you going?";
@@ -31,7 +33,21 @@ export default function BookingSearchBarMobile({ className }: Props) {
 	}, [checkInDate, checkOutDate]);
 
 	return (
-		<Popover open={isOpen} onOpenChange={setIsOpen}>
+		<Popover
+			open={isOpen}
+			onOpenChange={() => {
+				if (!isOpen) {
+					setStep("dates");
+					setIsOpen(true);
+					return;
+				}
+				if (isOpen && step === "dates") {
+					setStep("guests");
+					return;
+				}
+				setIsOpen(!isOpen);
+			}}
+		>
 			<PopoverTrigger
 				render={
 					<Button
@@ -54,25 +70,98 @@ export default function BookingSearchBarMobile({ className }: Props) {
 				/>
 			</PopoverTrigger>
 			<PopoverPopup className={"w-80"}>
-				<PopoverTitle className="pb-2 text-center font-normal text-muted-foreground text-sm tracking-tight">
-					Select your travel dates
-				</PopoverTitle>
-				<Separator />
-				<BookingSearchBarCalendar className="mt-1.5" />
-				<PopoverClose
-					className={"mt-2"}
-					render={
-						<Button
-							variant={"outline"}
-							size={"sm"}
-							className="w-full text-sm"
-						/>
-					}
-				>
-					<Save />
-					Save my dates
-				</PopoverClose>
+				{step === "dates" ? (
+					<PopoverDatesContent onSave={() => setStep("guests")} />
+				) : (
+					<PopoverGuestsContent
+						onSave={() => setIsOpen(false)}
+						setStep={setStep}
+					/>
+				)}
 			</PopoverPopup>
 		</Popover>
 	);
 }
+
+const PopoverDatesContent = ({
+	className,
+	onSave,
+}: {
+	className?: string;
+	onSave: () => void;
+}) => {
+	return (
+		<motion.div
+			className={cn("flex flex-col", className)}
+			initial={{ opacity: 0, y: -10 }}
+			animate={{ opacity: 1, y: 0 }}
+			exit={{ opacity: 0, y: -10 }}
+			transition={{ duration: 0.2 }}
+		>
+			<PopoverTitle className="pb-2 text-center font-normal text-muted-foreground text-sm tracking-tight">
+				Select your travel dates
+			</PopoverTitle>
+			<Separator />
+			<BookingSearchBarCalendar className="mt-1.5" />
+			<PopoverClose
+				className={"mt-2"}
+				render={
+					<Button
+						variant={"outline"}
+						size={"sm"}
+						className="w-full text-sm"
+						onClick={onSave}
+					/>
+				}
+			>
+				Save my dates
+			</PopoverClose>
+		</motion.div>
+	);
+};
+
+const PopoverGuestsContent = ({
+	className,
+	onSave,
+	setStep,
+}: {
+	className?: string;
+	onSave: () => void;
+	setStep: (step: "dates" | "guests") => void;
+}) => {
+	return (
+		<motion.div
+			className={cn("flex flex-col gap-2", className)}
+			initial={{ opacity: 0, y: -10 }}
+			animate={{ opacity: 1, y: 0 }}
+			exit={{ opacity: 0, y: -10 }}
+			transition={{ duration: 0.2 }}
+		>
+			<PopoverTitle className="text-center font-normal text-muted-foreground text-sm tracking-tight">
+				Select your guests count
+			</PopoverTitle>
+
+			<BookingGuestCountInput />
+			<PopoverClose
+				render={
+					<Button
+						variant={"default"}
+						size={"sm"}
+						className="w-full text-sm"
+						onClick={onSave}
+					/>
+				}
+			>
+				Save my guests
+			</PopoverClose>
+			<Button
+				variant={"outline"}
+				size={"sm"}
+				className="w-full text-sm"
+				onClick={() => setStep("dates")}
+			>
+				Update my dates
+			</Button>
+		</motion.div>
+	);
+};
--- apps/web/src/features/hotels/HotelPage.tsx
+++ apps/web/src/features/hotels/HotelPage.tsx
@@ -17,7 +17,7 @@ const HotelPage = ({ hotel }: Props) => {
 		<HotelProvider hotel={hotel}>
 			<div className="space-y-3">
 				<HotelBreadcrumb />
-				<div className="flex w-full flex-col gap-6 pb-15 md:flex-row">
+				<div className="flex w-full flex-col gap-6 pb-20 md:flex-row">
 					<div className="flex min-w-0 shrink flex-col gap-6">
 						<HotelCarousel />
 						<div className="space-y-1">
