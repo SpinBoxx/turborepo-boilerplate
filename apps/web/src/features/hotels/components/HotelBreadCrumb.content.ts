09bcb772feff29134cb3e609732d37735263cd37
Spinbox <quentin.mimault@orange.fr>
2026-03-19 02:45:43 +0100

    wip feat(web): intlayer => update key of hotelbreadcrumb

--- apps/web/src/features/hotels/components/HotelBreadCrumb.content.ts
+++ apps/web/src/features/hotels/components/HotelBreadCrumb.content.ts
@@ -3,6 +3,11 @@ import { type Dictionary, t } from "intlayer";
 const hotelBreadcrumbContent = {
 	key: "hotel-breadcrumb",
 	content: {
+		selectRooms: t({
+			fr: "Choisir une chambre",
+			en: "Select a room",
+			mg: "Misafidiana efitra",
+		}),
 		home: t({
 			fr: "Accueil",
 			en: "Home",
